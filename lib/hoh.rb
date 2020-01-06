8a46e213559e5216b063abd8b067d47b0940b2ce
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-06 15:52:53 -0800

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,20 +31,21 @@
 # }
 
 def naming_system
-  naming_system = {
-    label: "Kingdom",
-    sub_category: {
-      label: "Phylum",
-      sub_category: {
-        label: "Class",
-        sub_category: {
-          label: "Order", 
-          sub_category: {
-            label: "Family",
-            sub_category: {
-              label: "Genus",
-              sub_category: {
-                label: "Species"
+    naming_system = {
+             label: "Kingdom",
+             sub_category: {
+             label: "Phylum",
+             sub_category: {
+             label: "Class",
+             sub_category: { 
+             label: "Order", 
+             sub_category: {
+             label: "Family", 
+             sub_category: {
+             label: "Genus", 
+             sub_category: { 
+             label: "Species",
+             sub_category: nil  
               }
             }
           }
@@ -52,4 +53,4 @@ def naming_system
       }
     }
   }
-end
+end 
